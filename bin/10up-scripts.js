cfbb590c08e0e6820f3648ec62bd6d25611f1e06
Nícholas André <nicholas.andre@10up.com>
2021-01-21 10:47:02 -0300

    -v|--version option

--- bin/10up-scripts.js
+++ bin/10up-scripts.js
@@ -3,8 +3,16 @@
 /**
  * Internal dependencies
  */
-const { getNodeArgsFromCLI, spawnScript } = require('../utils');
+const { getNodeArgsFromCLI, spawnScript, getPackage } = require('../utils');
+const { exit } = require('../utils/process');
 
 const { scriptName, scriptArgs, nodeArgs } = getNodeArgsFromCLI();
+
+if (scriptArgs.includes('--version') || (scriptArgs.includes('-v') && !scriptName)) {
+	const pkg = getPackage();
+	// eslint-disable-next-line no-console
+	console.log('Version:', pkg.version);
+	exit(0);
+}
 
 spawnScript(scriptName, scriptArgs, nodeArgs);
