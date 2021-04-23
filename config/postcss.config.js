d79ef932817ca98ecaf359bb493f0a680b1bf43c
Daryll Doyle <daryll.doyle@10up.com>
2021-04-23 12:36:50 +0100

    Fix eslint issues

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -20,35 +20,32 @@ module.exports = ( { file, options, env } ) => { /* eslint-disable-line */
 	if (path.basename(file) === 'editor-style.css') {
 		config.plugins['postcss-editor-styles'] = {
 			scopeTo: '.editor-styles-wrapper',
-			ignore: [
-				':root',
-				'.edit-post-visual-editor.editor-styles-wrapper',
-				'.wp-toolbar',
-			],
+			ignore: [':root', '.edit-post-visual-editor.editor-styles-wrapper', '.wp-toolbar'],
 			remove: ['html', ':disabled', '[readonly]', '[disabled]'],
 			tags: ['button', 'input', 'label', 'select', 'textarea', 'form'],
 		};
 	}
 
-	config.plugins.cssnano = env === 'production'
-		? {
-			preset: [
-				'default',
-				{
-					autoprefixer: false,
-					calc: {
-						precision: 8,
-					},
-					convertValues: true,
-					discardComments: {
-						removeAll: true,
-					},
-					mergeLonghand: false,
-					zindex: false,
-				},
-			],
-		}
-		: false;
+	config.plugins.cssnano =
+		env === 'production'
+			? {
+					preset: [
+						'default',
+						{
+							autoprefixer: false,
+							calc: {
+								precision: 8,
+							},
+							convertValues: true,
+							discardComments: {
+								removeAll: true,
+							},
+							mergeLonghand: false,
+							zindex: false,
+						},
+					],
+			  }
+			: false;
 
 	return config;
 };
