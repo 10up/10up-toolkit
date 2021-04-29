035bd489b4ef9853a53e0dfe31ad9627086b44c7
Nícholas André <nicholas.andre@10up.com>
2021-04-29 11:33:41 -0300

    Normalizing snapshot

--- test-utils/webpack-serializer.js
+++ test-utils/webpack-serializer.js
@@ -1,29 +1,64 @@
+/**
+ * Checks if the provided value is a string
+ *
+ * @param {*} val The value to check.
+ *
+ * @returns {boolean}
+ */
 const isString = (val) => val && typeof val === 'string';
 
+/**
+ * Checks if the provided value has a local path
+ *
+ * @param {string} val The value to check.
+ *
+ * @returns {boolean}
+ */
 const hasLocalPath = (val) => val.indexOf(process.cwd()) !== -1;
 
-const isSpecialWebpackPlugin = (val) => {
-	return (
-		typeof val === 'object' &&
-		(val?.key ||
-			val?.experimentalUseImportModule ||
-			val?.maxConcurrency ||
-			val?.options?.experimentalUseImportModule)
-	);
+/**
+ * Checks if the provided value is a webpack plugin
+ *
+ * @param {*} val The value to check.
+ *
+ * @returns {boolean}
+ */
+const isWebPackPlugin = (val) => {
+	return typeof val === 'object' && val?.apply && typeof val?.apply === 'function';
+};
+
+/**
+ * Removes local path from the given value.
+ *
+ * @param {string} val The value to remove local path from.
+ *
+ * @returns {string}
+ */
+const removeLocalPath = (val) => {
+	return val.replace(new RegExp(process.cwd(), 'ig'), '');
+};
+
+/**
+ * Prints a normalized version of a webpack plugin object.
+ *
+ * @param {object} plugin The webpack plugin.
+ *
+ * @returns {string}
+ */
+const printWebPackPlugin = (plugin) => {
+	return `${plugin?.name || plugin?.constructor?.name}: ${removeLocalPath(
+		JSON.stringify(plugin?.options || {}),
+	)}`;
 };
 
 module.exports = {
 	serialize(val) {
 		if (isString(val) && hasLocalPath(val)) {
-			return `"${val.replace(process.cwd(), '')}"`;
+			return `"${removeLocalPath(val)}"`;
 		}
 
-		if (isSpecialWebpackPlugin(val)) {
-			delete val?.key;
-			delete val?.experimentalUseImportModule;
-			delete val?.maxConcurrency;
-			delete val?.options?.experimentalUseImportModule;
-			return JSON.stringify(val);
+		if (isWebPackPlugin(val)) {
+			return printWebPackPlugin(val);
 		}
 
 		return val;
@@ -31,7 +66,7 @@ module.exports = {
 
 	test(val) {
 		let shouldProcess = isString(val) && hasLocalPath(val);
-		shouldProcess ||= isSpecialWebpackPlugin(val);
+		shouldProcess ||= isWebPackPlugin(val);
 
 		return shouldProcess;
 	},
