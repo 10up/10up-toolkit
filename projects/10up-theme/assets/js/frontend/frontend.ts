e0c3b7aa0c27121e4eb65ca921e0f1e93d527dcd
Nícholas André <nicholas.andre@10up.com>
2021-05-21 17:50:45 -0300

    Updates

--- projects/10up-theme/assets/js/frontend/frontend.ts
+++ projects/10up-theme/assets/js/frontend/frontend.ts
@@ -1,7 +1,9 @@
-// import foo from './components/bar';
+import { Accordion } from '@10up/component-accordion';
 
 function helloTypeScript(name: string) {
 	return `Hello ${name}, this is TypeScript!`;
 }
 
 document.write(helloTypeScript('10up'));
+
+new Accordion('.test', {});
