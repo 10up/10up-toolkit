a68a92621370141807a8e159e1addab1a7b5430c
Nícholas André <nicholas.andre@10up.com>
2021-09-15 14:37:42 -0300

    Update browser sync config. props @saltnpixels and @saltcod

--- packages/toolkit/config/webpack/plugins.js
+++ packages/toolkit/config/webpack/plugins.js
@@ -79,9 +79,17 @@ module.exports = ({
 					port: getArgFromCLI('--port') || 3000,
 					proxy: devURL,
 					open: false,
-					files: ['**/*.php', 'dist/**/*.css'],
-					ignore: ['dist/**/*.php'],
+					files: ['**/*.php', '**/*.js', 'dist/**/*.css'],
+					ignore: ['dist/**/*.php', 'dist/**/*.js'],
+					serveStatic: ['.'],
+					rewriteRules: [
+						{
+							match: /wp-content\/themes\/.*\/dist/g,
+							replace: 'dist',
+						},
+					],
 				},
+
 				{
 					injectCss: true,
 					reload: false,
