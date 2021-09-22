86fe95f7569be35003544ef7d8256ba2a13e62af
Nícholas André <nicholas.andre@10up.com>
2021-09-22 20:14:47 -0300

    Removing unnecessary files

--- packages/toolkit/webpack-deps.js
+++
@@ -1,7 +0,0 @@
-module.exports = function () {
-	return {
-		webpack: require('webpack'),
-		'webpack-dev-server': require('webpack-dev-server'),
-		'mini-css-extract-plugin': require('mini-css-extract-plugin'),
-	};
-};
