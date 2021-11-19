b9080a9f9420ae9736ebfdf0397229c11687a360
Nícholas André <nicholas.andre@10up.com>
2021-11-18 22:44:33 -0300

    Basic postcss tests

--- packages/toolkit/config/__tests__/postscss.config.js
+++ packages/toolkit/config/__tests__/postscss.config.js
@@ -1,4 +1,8 @@
+const postcss = require('postcss');
+const fs = require('fs');
+const path = require('path');
 const configFactory = require('../postcss.config');
+const { loadPostCSSPlugins } = require('../../test-utils/load-postcss-plugins');
 
 describe('postcss.config.js', () => {
 	it('builds a proper postcss config file for editor styles', () => {
@@ -31,5 +35,36 @@ describe('postcss.config.js', () => {
 				env: 'development',
 			}),
 		).toMatchSnapshot('env=development');
+	});
+});
+
+describe('postcss', () => {
+	const config = configFactory({
+		// this is just to control the postcss config based on file name
+		file: 'style.css',
+		env: 'production',
+	});
+
+	const postCSSPlugins = loadPostCSSPlugins(config);
+
+	it('properly transforms css', async () => {
+		const css = fs.readFileSync(path.join(__dirname, '__fixtures__', 'styleguide.css'));
+		const result = await postcss(postCSSPlugins).process(css, {
+			from: 'styleguide.css',
+			to: 'styleguide.out.css',
+		});
+
+		expect(result.css).toMatchSnapshot();
+	});
+
+	it('transforms mixins properly', async () => {
+		const css = fs.readFileSync(path.join(__dirname, '__fixtures__', 'mixins.css'));
+
+		const result = await postcss(postCSSPlugins).process(css, {
+			from: 'mixins.css',
+			to: 'mixins.out.css',
+		});
+
+		expect(result.css).toMatchSnapshot();
 	});
 });
