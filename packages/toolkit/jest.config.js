84433589db483f1b0aedc91a29d6b28b396bcab2
Nícholas André <nicholas.andre@10up.com>
2021-11-20 17:24:01 -0300

    Update snapshots

--- packages/toolkit/jest.config.js
+++ packages/toolkit/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	testEnvironment: 'jsdom',
+	testEnvironment: 'node',
 	testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/test/*.[jt]s?(x)', '**/?(*.)test.[jt]s?(x)'],
 	testPathIgnorePatterns: ['/node_modules/', '<rootDir>/vendor/'],
 	setupFilesAfterEnv: ['@wordpress/jest-console'],
