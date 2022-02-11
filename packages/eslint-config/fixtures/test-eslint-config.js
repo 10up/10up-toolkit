e37293a48bb63f02534791db2d5eb06584ef7c64
Nícholas André <nicholas.andre@10up.com>
2022-02-11 18:28:08 -0300

    fixing eslint tests

--- packages/eslint-config/fixtures/test-eslint-config.js
+++ packages/eslint-config/fixtures/test-eslint-config.js
@@ -4,22 +4,22 @@ process.on('unhandledRejection', (err) => {
 });
 
 const { join } = require('path');
-const { CLIEngine } = require('eslint');
+const { ESLint } = require('eslint');
 const { getConfigFile, countResults } = require('./helpers');
 
-const configFile = getConfigFile();
-const directoryToTest = configFile.replace('.js', '');
-const cli = new CLIEngine({ useEslintrc: false, configFile });
-const formatter = CLIEngine.getFormatter();
+const baseConfig  = getConfigFile();
+const directoryToTest = baseConfig .replace('.js', '');
+const cli = new ESLint({ useEslintrc: false, overrideConfigFile : baseConfig  });
+const formatter = cli.loadFormatter();
 
 const verbose = process.argv.indexOf('--verbose') > -1;
 
 console.log('Running ESLint on fixtures directories. Use --verbose for a detailed report.');
 console.log(`\nLinting ${join(__dirname, directoryToTest, '/fail/*.js')}...`);
 
-const antipatternReport = cli.executeOnFiles([join(__dirname, directoryToTest, '/fail/*.js')]);
-const antipatternCounts = countResults(antipatternReport.results);
-const allFail = antipatternReport.results.reduce(
+cli.lintFiles([join(__dirname, directoryToTest, '/fail/*.js')]).then((results) => {
+	const antipatternCounts = countResults(results);
+	const allFail = results.reduce(
 	// eslint-disable-next-line no-return-assign,no-param-reassign
 	(didFail, file) => didFail && (file.errorCount > 0 || file.warningCount > 0),
 	true,
@@ -29,7 +29,7 @@ if (allFail) {
 	console.log('√ ESLint logs errors as expected.\n');
 } else if (antipatternCounts.errors) {
 	console.log('The following files did not produce errors:');
-	antipatternReport.results.forEach((file) => {
+	results.forEach((file) => {
 		if (file.errorCount > 0 || file.warningCount > 0) {
 			return;
 		}
@@ -49,23 +49,26 @@ if (verbose) {
 	console.log(formatter(antipatternReport.results));
 }
 
+});
+
 // Run for pass tests
 
 console.log(`\nLinting ${join(__dirname, directoryToTest, '/pass/*.js')}...`);
 
-const exampleReport = cli.executeOnFiles([join(__dirname, directoryToTest, '/pass/*.js')]);
-const exampleCounts = countResults(exampleReport.results);
+cli.lintFiles([join(__dirname, directoryToTest, '/pass/*.js')]).then((results) => {
+	const exampleCounts = countResults(results);
 
-// Log full report when --verbose, or whenever errors are unexpectedly reported.
-if (verbose || exampleCounts.errors || exampleCounts.warnings) {
-	console.log(formatter(exampleReport.results));
-}
+	// Log full report when --verbose, or whenever errors are unexpectedly reported.
+	if (verbose || exampleCounts.errors || exampleCounts.warnings) {
+		console.log(formatter(results));
+	}
 
-if (exampleCounts.errors) {
-	const { errors } = exampleCounts;
-	console.log(`${errors} unexpected error${errors !== 1 ? 's' : ''}!\n`);
-	process.exitCode = 1;
-} else {
-	const { files } = exampleCounts;
-	console.log(`${files} files pass lint.`);
-}
+	if (exampleCounts.errors) {
+		const { errors } = exampleCounts;
+		console.log(`${errors} unexpected error${errors !== 1 ? 's' : ''}!\n`);
+		process.exitCode = 1;
+	} else {
+		const { files } = exampleCounts;
+		console.log(`${files} files pass lint.`);
+	}
+});
