cf9c4400bcb96b4f8d58c2ef160dee939543be7d
Nícholas André <nicholas.andre@10up.com>
2022-02-21 18:00:21 -0300

    publicPath does not seem necessary

--- packages/toolkit/config/webpack/output.js
+++ packages/toolkit/config/webpack/output.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = ({ isPackage, projectConfig: { filenames, hot, devURl }, buildFiles }) => {
+module.exports = ({ isPackage, projectConfig: { filenames }, buildFiles }) => {
 	if (isPackage) {
 		return {
 			path: path.resolve(process.cwd(), 'dist'),
@@ -9,7 +9,6 @@ module.exports = ({ isPackage, projectConfig: { filenames, hot, devURl }, buildF
 
 	return {
 		path: path.resolve(process.cwd(), 'dist'),
-		publicPath: hot ? devURl : undefined,
 		chunkFilename: filenames.jsChunk,
 		filename: (pathData) => {
 			return buildFiles[pathData.chunk.name].match(/\/blocks\//)
