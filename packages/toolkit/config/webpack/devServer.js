f8a43935abe20d38f76395cdc580f6dd05b149eb
Nícholas André <nicholas.andre@10up.com>
2022-02-22 09:22:49 -0300

    Fixing tests

--- packages/toolkit/config/webpack/devServer.js
+++ packages/toolkit/config/webpack/devServer.js
@@ -1,9 +1,9 @@
 module.exports = ({ isPackage, projectConfig: { devServer, hot, devServerPort } }) => {
-	if (!devServer || !hot) {
+	if (!devServer && !hot) {
 		return undefined;
 	}
 
-	if (isPackage) {
+	if (isPackage && devServer) {
 		return {
 			contentBase: 'public',
 			compress: true,
@@ -11,28 +11,32 @@ module.exports = ({ isPackage, projectConfig: { devServer, hot, devServerPort } 
 		};
 	}
 
-	return {
-		devMiddleware: {
-			writeToDisk: true,
-		},
-		client: {
-			webSocketURL: `ws://127.0.0.1:${devServerPort}/ws`,
-		},
-		headers: {
-			// Requests come from the WP port.
-			'Access-Control-Allow-Origin': '*',
-		},
-		// hot: true,
-		allowedHosts: 'all',
-		host: '0.0.0.0',
-		hot: true,
-		port: Number(devServerPort),
-		proxy: {
-			'/dist': {
-				pathRewrite: {
-					'^/dist': '',
+	if (!isPackage && hot) {
+		return {
+			devMiddleware: {
+				writeToDisk: true,
+			},
+			client: {
+				webSocketURL: `ws://127.0.0.1:${devServerPort}/ws`,
+			},
+			headers: {
+				// Requests come from the WP port.
+				'Access-Control-Allow-Origin': '*',
+			},
+			// hot: true,
+			allowedHosts: 'all',
+			host: '0.0.0.0',
+			hot: true,
+			port: Number(devServerPort),
+			proxy: {
+				'/dist': {
+					pathRewrite: {
+						'^/dist': '',
+					},
 				},
 			},
-		},
-	};
+		};
+	}
+
+	return undefined;
 };
