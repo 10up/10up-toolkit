8c9ae56a915276dc7521e8ac7752a2543e95be9e
Nícholas Oliveira <nicholas.andre@10up.com>
2022-02-26 02:25:10 -0300

    Dropping imagemin

--- packages/toolkit/config/webpack/modules.js
+++ packages/toolkit/config/webpack/modules.js
@@ -81,7 +81,7 @@ module.exports = ({
 			},
 			{
 				test: /\.svg$/,
-				use: ['@svgr/webpack', 'url-loader'],
+				use: ['svgo-loader', '@svgr/webpack', 'url-loader'],
 			},
 			{
 				test: /\.css$/,
@@ -128,6 +128,7 @@ module.exports = ({
 			isPackage && {
 				test: /\.(woff(2)?|ttf|eot|svg|jpg|jpeg|png|giff|webp)(\?v=\d+\.\d+\.\d+)?$/,
 				type: 'asset/resource',
+				use: [{ loader: 'svgo-loader' }],
 			},
 		].filter(Boolean),
 	};
