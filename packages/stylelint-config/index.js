c3a43be8f921a6bc4ed7657c4d0c5a2a3b0bb157
Antonio Laguna <antonio.laguna@10up.com>
2022-03-23 18:41:10 +0100

    Updating stylelint

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: '@wordpress/stylelint-config',
+	extends: ['stylelint-config-standard-scss', '@wordpress/stylelint-config'],
 	plugins: ['stylelint-declaration-strict-value', 'stylelint-order'],
 	rules: {
 		'scale-unlimited/declaration-strict-value': [
