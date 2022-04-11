4315f3fe6412ff6d14be9e4f432b4cd3201d458f
Nícholas André <nicholas.andre@10up.com>
2022-04-11 14:05:32 -0300

    feature: esm output

--- projects/library-ts/src/index.ts
+++ projects/library-ts/src/index.ts
@@ -2,6 +2,10 @@ import './style.css';
 
 export * from './hello-world';
 
+export function sayIndex() {
+	console.log('Hello from index.ts');
+}
+
 export interface MyInterface {
 	name: string;
 	value: string;
