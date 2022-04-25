a1b8512ecd979e44cbe0b728557c2e4e2e635627
Nícholas André <nicholas.andre@10up.com>
2022-04-25 20:00:50 -0300

    example for using react on the front-end

---
+++ projects/10up-theme/assets/js/frontend/frontend.js
@@ -0,0 +1,9 @@
+import '../../css/frontend/style.css';
+// eslint-disable-next-line
+import ReactDOM from 'react-dom';
+
+const App = () => {
+	return <p>This is a react app</p>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
