8deb2b8eef8736f196a10f88a6ed08fa1bef7400
Nícholas André <nicholas.andre@10up.com>
2022-06-08 18:58:39 -0300

    Checking for .local domains as well and update docs

--- projects/10up-theme/functions.php
+++ projects/10up-theme/functions.php
@@ -14,9 +14,11 @@ define( 'TENUP_THEME_DIST_URL', TENUP_THEME_TEMPLATE_URL . '/dist/' );
 define( 'TENUP_THEME_INC', TENUP_THEME_PATH . 'includes/' );
 define( 'TENUP_THEME_BLOCK_DIR', TENUP_THEME_INC . 'blocks/' );
 
-$is_local_env = in_array( wp_get_environment_type(), [ 'local', 'development' ], true ) || strpos( home_url(), '.test' );
+$is_local_env = in_array( wp_get_environment_type(), [ 'local', 'development' ], true );
+$is_local_url = strpos( home_url(), '.test' ) || strpos( home_url(), '.local' );
+$is_local     = $is_local_env || $is_local_url;
 
-if ( $is_local_env && file_exists( __DIR__ . '/dist/fast-refresh.php' ) ) {
+if ( $is_local && file_exists( __DIR__ . '/dist/fast-refresh.php' ) ) {
 	require_once __DIR__ . '/dist/fast-refresh.php';
 	TenUpToolkit\set_dist_url_path( basename( __DIR__ ), TENUP_THEME_DIST_URL, TENUP_THEME_DIST_PATH );
 }
