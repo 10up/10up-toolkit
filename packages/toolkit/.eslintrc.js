806de8183c3b832b8e51161dc4c2382a9716c485
Antonio Laguna <antonio.laguna@10up.com>
2022-08-08 12:05:42 +0200

    Toolkit too needs Jest

--- packages/toolkit/.eslintrc.js
+++ packages/toolkit/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: '@10up/eslint-config/node',
+	extends: ['@10up/eslint-config/node', '@10up/eslint-config/jest'],
 	rules: {
 		'global-require': 'off',
 		'import/no-dynamic-require': 'off',
