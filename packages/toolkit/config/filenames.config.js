7591fe0e38fb1300125452d9da3439bce499ef5a
Fabian Kägy <mail@fabian-kaegy.de>
2022-09-07 12:12:26 +0200

    fix only use new block path if useBlockAssets flag is set

--- packages/toolkit/config/filenames.config.js
+++ packages/toolkit/config/filenames.config.js
@@ -1,7 +1,7 @@
-module.exports = {
+module.exports = ({ projectConfig: { useBlockAssets } }) => ({
 	js: 'js/[name].js',
 	jsChunk: 'js/[name].[contenthash].chunk.js',
 	css: 'css/[name].css',
-	block: 'blocks/[name].js',
-	blockCSS: 'blocks/[name].css',
-};
+	block: useBlockAssets ? 'blocks/[name].js' : 'blocks/[name]/editor.js',
+	blockCSS: useBlockAssets ? 'blocks/[name].css' : 'blocks/[name]/editor.css',
+});
