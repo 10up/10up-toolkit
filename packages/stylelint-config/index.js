dbe333853aed9e81e9d0e87d17208330a6ae2a79
Antonio Laguna <antonio.laguna@10up.com>
2023-01-20 13:47:30 +0100

    Ensuring SCSS rules also ignore mixin and define-mixin
    Fixes #226

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -13,5 +13,6 @@ module.exports = {
 		'selector-nested-pattern': ['^&'],
 		'no-descending-specificity': null,
 		'at-rule-no-unknown': [true, { ignoreAtRules: ['mixin', 'define-mixin'] }],
+		'scss/at-rule-no-unknown': [true, { ignoreAtRules: ['mixin', 'define-mixin'] }],
 	},
 };
