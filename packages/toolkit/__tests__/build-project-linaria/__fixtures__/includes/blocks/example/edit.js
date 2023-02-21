6be1af96869d05809dc391a8573ed1f92a6e4fd6
Nícholas Oliveira <nicholas.andre@10up.com>
2023-02-21 17:33:54 -0300

    add tests for linaria

---
+++ packages/toolkit/__tests__/build-project-linaria/__fixtures__/includes/blocks/example/edit.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import { css } from '@linaria/core';
+
+const className = css`
+	border: 2px dashed black;
+	background-color: red;
+`;
+
+const ExampleBlockEdit = () => {
+	return <div className={className} />
+};
+
+export default ExampleBlockEdit;
