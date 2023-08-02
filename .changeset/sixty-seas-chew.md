b17208197359a765d2960ccfdb41538e60f2d7f5
Antonio Laguna <antonio.laguna@10up.com>
2023-08-02 13:58:18 +0200

    Updating rule to work with WP style (#314)

---
+++ .changeset/sixty-seas-chew.md
@@ -0,0 +1,6 @@
+---
+"@10up/stylelint-config": patch
+---
+
+Fixes validation for `--wp--some--property` for custom properties
+Fixes usage of `currentcolor` vs `currentColor`
--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -4,7 +4,7 @@ module.exports = {
 	rules: {
 		'scale-unlimited/declaration-strict-value': [
 			'/color/',
-			{ ignoreValues: ['currentColor', 'inherit', 'initial', 'transparent', 'unset'] },
+			{ ignoreValues: ['currentcolor', 'inherit', 'initial', 'transparent', 'unset'] },
 		],
 		'order/properties-alphabetical-order': true,
 		'function-url-quotes': 'always',
@@ -13,5 +13,11 @@ module.exports = {
 		'selector-nested-pattern': ['^&'],
 		'no-descending-specificity': null,
 		'at-rule-no-unknown': [true, { ignoreAtRules: ['mixin', 'define-mixin'] }],
+		'custom-property-pattern': [
+			'^([a-z][a-z0-9]*)(-[a-z0-9]+)*$|^wp--([a-z][a-z0-9]*)(--[a-z0-9]+)*$',
+			{
+				message: 'Expected custom property name to be kebab-case or wp--kebab--case',
+			},
+		],
 	},
 };
