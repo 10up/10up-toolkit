8b74e5a02bdedc2e62b03d338c119629be2afd14
Dustin Rue <ruedu@dustinrue.com>
2024-08-21 14:36:32 -0500

    Feature/add more flexibility (#413)
    Co-authored-by: Taylor Lovett <tlovett88@gmail.com>

---
+++ packages/toolkit/scripts/project/update-composer.js
@@ -0,0 +1,35 @@
+const { execSync } = require('child_process');
+const chalk = require('chalk');
+
+const { log } = console;
+
+const { getProjectRoot, getProjectVariables, setEnvVariables } = require('../../utils');
+
+const description = '10up-toolkit project update-composer';
+
+const run = async () => {
+	const root = getProjectRoot();
+
+	if (!root) {
+		log(chalk.red('This is not a project.'));
+		process.exit(1);
+	}
+
+	// combine project variables with actual environment variables
+	const variables = { ...getProjectVariables(), ...process.env };
+
+	if (!variables) {
+		log(chalk.red('No .tenup.yml found.'));
+		process.exit(1);
+	}
+
+	setEnvVariables(variables);
+
+	execSync(`bash -l ${__dirname}/bash/scripts.sh update-composer`, {
+		stdio: 'inherit',
+	});
+
+	log(chalk.green('Composer update complete.'));
+};
+
+module.exports = { run, description };
