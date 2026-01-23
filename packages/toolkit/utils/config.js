d28a27e465796bfab302b65cf8d93212be564629
Fabian Kägy <mail@fabian-kaegy.de>
2026-01-23 08:42:19 +0100

    feat(toolkit): add useBlockManifest config option
    Add useBlockManifest configuration option and --block-manifest CLI flag support. When enabled, toolkit will generate a block metadata manifest file during builds.
    
    Defaults to false to maintain backward compatibility.

--- packages/toolkit/utils/config.js
+++ packages/toolkit/utils/config.js
@@ -110,6 +110,7 @@ const getDefaultConfig = () => {
 	const include = hasArgInCLI('--include') ? getArgFromCLI('--include').split(',') : [];
 	const sourcemap = hasArgInCLI('--sourcemap');
 	const useScriptModules = hasArgInCLI('--block-modules') || false;
+	const useBlockManifest = hasArgInCLI('--block-manifest') || false;
 
 	const buildFilesPath = hasProjectFile('buildfiles.config.js')
 		? fromProjectRoot('buildfiles.config.js')
@@ -142,6 +143,7 @@ const getDefaultConfig = () => {
 		loadBlockSpecificStyles: false,
 		useBlockAssets: true,
 		useScriptModules,
+		useBlockManifest,
 		include,
 	};
 };
